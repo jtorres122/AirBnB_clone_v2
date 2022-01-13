aeb43957301900d35005efadd183f88447433ec5
Julian <jose.torres122@outlook.com>
2022-01-13 19:17:00 -0400

    unlucky

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 # Bash script sets up web servers for deployment of web_static
 
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+apt-get -y update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
 mkdir -p ~/data/web_static/{releases/test,shared}
-echo "<html>
-  <head>
-    Holberton School
-  </head>
-  <body>
-    AirBnB clone deployed
-  </body>
-</html>" > /data/web_static/releases/test/index.html
+echo "Deployment of web_static" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+chown -hR ubuntu:ubuntu /data/
 sed -i "42i\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-sudo service nginx restart
-exit 0
+service nginx restart
