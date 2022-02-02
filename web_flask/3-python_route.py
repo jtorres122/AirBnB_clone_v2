e973716f3551743584cc14ed1071395ec3976d9f
Julian <jose.torres122@outlook.com>
2022-02-02 10:01:38 -0400

    Script adds a route with a default value

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''Module starts a Flask web app'''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    '''Function returns msg when web app is curled'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    '''Function returns msg when web app is curled'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def hello_c(text):
+    '''Function returns msg when web app is curled'''
+    return 'C ' + str(text).replace("_", " ")
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def hello_python(text='is cool'):
+    '''Function returns msg when web app is curled'''
+    return 'Python ' + str(text).replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
