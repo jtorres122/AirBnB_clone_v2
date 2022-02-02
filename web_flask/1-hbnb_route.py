3ad2bcc38ef47f9656f03f8adb5293f06a6a3c98
Julian <jose.torres122@outlook.com>
2022-02-02 03:22:11 -0400

    Add a second route to flask web app

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Module starts a Flask web app'''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    '''Function returns msg when web app is curled'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    '''Function returns msg when web app is curled'''
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
