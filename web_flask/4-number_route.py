2a29f0472ae0a3cef0199a23d69f032aa2bfdbfd
Julian <jose.torres122@outlook.com>
2022-02-02 10:36:54 -0400

    Add a route that returns an int

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+'''Module starts a Flask web app'''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    '''Function returns msg when web app is curled'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_hbnb():
+    '''Function returns msg when web app is curled'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def hello_c(text):
+    '''Function returns msg when web app is curled'''
+    return 'C ' + str(text).replace("_", " ")
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def hello_python(text='is cool'):
+    '''Function returns msg when web app is curled'''
+    return 'Python ' + str(text).replace("_", " ")
+
+
+@app.route('/number/<int:n>')
+def hello_n(n):
+    '''Function returns n if it's a number'''
+    return str(n) + ' is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
