829b1295940c2801f31d5b1df0eeac3fa42af3ee
Julian <jose.torres122@outlook.com>
2022-02-02 03:14:03 -0400

    pep8 compliance

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,11 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.strict_slashes=False
+app.strict_slashes = False
 @app.route('/')
 def hello():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
